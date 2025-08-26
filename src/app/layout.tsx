a38cceb8c26a44e054b4d24c4aaf3b8cfd946e11
Michelle <missetubal@outlook.com>
2025-08-25 22:26:10 -0300

    🎉 Init v2 of website

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+import { JetBrains_Mono, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Header } from '../components/custom';
+
+const jetBrainsMono = JetBrains_Mono({
+  variable: '--font-jetbrainsMono',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
+
+export const metadata: Metadata = {
+  title: 'Michelle Setúbal',
+  description: 'Brazilian Software Developer',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={` ${jetBrainsMono.variable} antialiased`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+import { NavLink } from '@/types';
+
+export const navLinks: NavLink[] = [
+  {
+    title: 'About',
+    path: '#about',
+  },
+  {
+    title: 'Projects',
+    path: '#projects',
+  },
+  {
+    title: 'Contact',
+    path: '#contact',
+  },
+];
